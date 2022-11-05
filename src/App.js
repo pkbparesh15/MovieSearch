2840b532cacf1299267f8f6ee8186314128b48f5
Paresh Kumar Behura <pkbparesh15@gmail.com>
2022-11-05 19:33:34 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+//APP.JS
+
+
+import './App.css';
+import Movies from './Component/Movies/Movies';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/*Movie component*/}
+      <Movies />
+    </div>
+  );
+}
+
+export default App;
